908a21d05dee4ba85df63c380328dfda4af0fbe9
Alex <agkim@lbl.gov>
2023-03-23 10:09:09 -0400

    New functions for SNR based on diffstar parameters

---
+++ src/sn_diffstar/utils.py
@@ -0,0 +1,29 @@
+import jax
+from jax import jit as jjit
+from jax import vmap
+from jax import numpy as jnp
+
+from sn_diffstar.mspop import _ms_means_and_covs_lgmpop, _get_ms_means, get_ms_sfh_scan_tobs_lgm0, SFR_MIN
+
+@jjit
+def SNR(t0, A, beta, tp, mah_params_one):
+    mah_params_pop=mah_params_one[None,:]
+    
+    taus_p = jnp.linspace(jnp.power(tp, beta+1), jnp.power(15-t0, beta+1), 1000)
+    taus = jnp.power(taus_p, -(beta+1))
+    
+    lgmpop = mah_params_pop[:,0]
+#     means_ms_pop, cov_ms_pop = _ms_means_and_covs_lgmpop(lgmpop)
+
+    ms_u_params_pop = _get_ms_means(lgmpop)
+
+    ms_sfh_pop = get_ms_sfh_scan_tobs_lgm0(taus+t0, mah_params_pop, ms_u_params_pop)
+    ms_sfh_pop = jnp.where(ms_sfh_pop < SFR_MIN, SFR_MIN, ms_sfh_pop)
+
+#     _, ms_sfh_pop, _ = mc_galhalo_ms_lgmpop(ran_key, mah_params_pop, taus+t0)
+    return 1./(1+beta)*A*jnp.trapz(ms_sfh_pop,taus_p)
+
+SNR_t0  = jjit(vmap(SNR,  in_axes=(0, None, None, None, None)))
+SNR_mah  = jjit(vmap(SNR,  in_axes=(None, None, None, None, 0)))
+SNR_gal  = jjit(vmap(SNR,  in_axes=(0, None, None, None, 0)))
+
